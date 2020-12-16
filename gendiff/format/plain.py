13d23774dc2c9826c9122e1864c9556aaa3c5ae0
Anton <zavr35rus@gmail.com>
2020-12-16 21:41:08 +0300

    fixed linter errors

--- gendiff/format/plain.py
+++ gendiff/format/plain.py
@@ -3,12 +3,14 @@ def to_plain(diff: dict, parent=None):
     for key, (status, value) in sorted(diff.items()):
         path = get_whole_path(key, parent)
         if status == "ADDED":
-            strings.append(f"Property '{path}' was added with value: {check_value(value)}")
+            strings.append(f"Property '{path}' was added with value:"
+                           f" {check_value(value)}")
         elif status == "REMOVED":
             strings.append(f"Property '{path}' was removed")
         elif status == "CHANGED":
-            strings.append(f"Property '{path}' was updated. From {check_value(value[0])}"
-                           f" to {check_value(value[1])}")
+            strings.append(f"Property '{path}' was updated. "
+                           f"From {check_value(value[0])} "
+                           f"to {check_value(value[1])}")
         elif status == "NESTED":
             strings.append(to_plain(value, path))
     result = '\n'.join(strings)
