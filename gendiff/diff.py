28d60ee37fcfd03d1d01bf5d74b8f8f7dc9d3add
Anton <zavr35rus@gmail.com>
2020-12-22 15:21:51 +0300

    Reduced complexity in add_children finction

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -2,20 +2,26 @@ ADDED, REMOVED, CHANGED, NO_CHANGED, NESTED, ROOT = \
     'added', 'removed', 'changed', 'no changed', 'nested', 'root'
 
 
-def add_removed_key(key: str, file: dict) -> dict:
-    return {
-        'key': key,
-        'type': REMOVED,
-        'value': file[key]
-    }
+def add_removed_keys(file1: dict, file2: dict) -> list:
+    result = []
+    for key in file1.keys() - file2.keys():
+        result.append({
+            'key': key,
+            'type': REMOVED,
+            'value': file1[key]
+        })
+    return result
 
 
-def add_added_key(key: str, file: dict) -> dict:
-    return {
-        'key': key,
-        'type': ADDED,
-        'value': file[key]
-    }
+def add_added_keys(file1: dict, file2: dict) -> list:
+    result = []
+    for key in file2.keys() - file1.keys():
+        result.append({
+            'key': key,
+            'type': ADDED,
+            'value': file2[key]
+        })
+    return result
 
 
 def add_no_changed_key(key: str, file: dict) -> dict:
@@ -43,14 +49,10 @@ def add_nested_key(key: str, file1: dict, file2: dict) -> dict:
     }
 
 
-def add_children(data1: dict, data2: dict):
+def add_children(data1: dict, data2: dict) -> list:
     result = []
-
-    for key in data2.keys() - data1.keys():
-        result.append(add_added_key(key, data2))
-
-    for key in data1.keys() - data2.keys():
-        result.append(add_removed_key(key, data1))
+    result.extend(add_added_keys(data1, data2))
+    result.extend(add_removed_keys(data1, data2))
 
     for key in data1.keys() & data2.keys():
         if data1[key] == data2[key]:
