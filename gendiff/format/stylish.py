0b6def552a6c05585886212082309b21044e9c9b
Anton <zavr35rus@gmail.com>
2021-01-02 13:08:39 +0300

    Refactored without concatenation

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -1,12 +1,13 @@
 from gendiff.tree import ADDED, REMOVED, CHANGED, UNCHANGED, NESTED
 
 
-def prepare(value, indent):
+def prepare_value(value, indent):
     if isinstance(value, dict):
         return prepare_dict(value, indent)
+    elif isinstance(value, bool):
+        return str(value).lower()
     else:
-        return str(value).replace('True', 'true').replace('False', 'false').\
-            replace('None', 'null')
+        return str(value).replace('None', 'null')
 
 
 def prepare_dict(value, indent):
@@ -14,7 +15,7 @@ def prepare_dict(value, indent):
     result = '{\n'
     for key, value in value.items():
         if isinstance(value, dict):
-            result += f'{spacer}{key}: {prepare(value, indent + 4)}\n'
+            result += f'{spacer}{key}: {prepare_value(value, indent + 4)}\n'
         else:
             result += f'{spacer}{key}: {value}\n'
     else:
@@ -22,23 +23,35 @@ def prepare_dict(value, indent):
     return result
 
 
-def transform(diff: dict, indent=2):
+def prepares_changes(tree: dict, indent=2) -> list:
+    strings = []
     spacer = ' ' * indent
-    result = '{\n'
-    for n in diff['children']:
-        if n['type'] == ADDED:
-            result += f'{spacer}+ {n["key"]}: {prepare(n["value"], indent)}\n'
-        elif n['type'] == REMOVED:
-            result += f'{spacer}- {n["key"]}: {prepare(n["value"], indent)}\n'
-        elif n['type'] == CHANGED:
-            result += \
-                f'{spacer}- {n["key"]}: {prepare(n["old_value"], indent)}\n'
-            result += \
-                f'{spacer}+ {n["key"]}: {prepare(n["new_value"], indent)}\n'
-        elif n['type'] == UNCHANGED:
-            result += f'{spacer}  {n["key"]}: {prepare(n["value"], indent)}\n'
-        elif n['type'] == NESTED:
-            result += \
-                f'{spacer}  {n["key"]}: {transform(n, indent=indent + 4)}\n'
-    result += f'{spacer[2:]}' + '}'
-    return result
+
+    for node in tree['children']:
+        if node['type'] == ADDED:
+            strings.append(f'{spacer}+ {node["key"]}: '
+                           f'{prepare_value(node["value"], indent)}')
+        elif node['type'] == REMOVED:
+            strings.append(f'{spacer}- {node["key"]}: '
+                           f'{prepare_value(node["value"], indent)}')
+        elif node['type'] == CHANGED:
+            strings.append(f'{spacer}- {node["key"]}: '
+                           f'{prepare_value(node["old_value"], indent)}')
+            strings.append(f'{spacer}+ {node["key"]}: '
+                           f'{prepare_value(node["new_value"], indent)}')
+        elif node['type'] == UNCHANGED:
+            strings.append(f'{spacer}  {node["key"]}: '
+                           f'{prepare_value(node["value"], indent)}')
+        elif node['type'] == NESTED:
+            strings.append(f'{spacer}  {node["key"]}: {chr(123)}')
+            strings.extend(prepares_changes(node, indent=indent + 4))
+            strings.append(f'{spacer}  {chr(125)}')
+
+    return strings
+
+
+def transform(diff: dict) -> str:
+    changes = ['{']
+    changes.extend(prepares_changes(diff))
+    changes.append('}')
+    return '\n'.join(changes)
