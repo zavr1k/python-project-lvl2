7dd5bd4d99de7a2db0fdcf16079026935f8db42d
Anton <zavr35rus@gmail.com>
2021-01-06 20:26:08 +0300

    Refactored with f strings

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -11,26 +11,26 @@ def prepare_value(value, indent):
 
 
 def prepare_dict(value, indent):
-    strings = []
-    strings.extend(prepare_strings(value, indent))
-    return '\n'.join(strings)
+    spacer = ' ' * indent
+    string = '\n'.join(prepare_strings(value, indent))
+    return f'{{\n{string}\n{spacer}  }}'
 
 
 def prepare_strings(value, indent):
     spacer = ' ' * (indent + 6)
-    strings = ['{']
+    strings = []
     for key, value in value.items():
         if isinstance(value, dict):
             strings.append(f'{spacer}{key}: {{')
-            strings.extend(prepare_strings(value, indent + 4)[1:])
+            strings.extend(prepare_strings(value, indent + 4))
+            strings.append(f'{spacer}}}')
         else:
             strings.append(f'{spacer}{key}: {value}')
-    strings.append(f'{spacer[4:]}}}')
     return strings
 
 
 def prepares_changes(tree: dict, indent=2) -> list:
-    strings = ['{']
+    strings = []
     spacer = ' ' * indent
 
     for node in tree['children']:
@@ -50,12 +50,11 @@ def prepares_changes(tree: dict, indent=2) -> list:
                            f'{prepare_value(node["value"], indent)}')
         elif node['type'] == NESTED:
             strings.append(f'{spacer}  {node["key"]}: {{')
-            strings.extend(prepares_changes(node, indent=indent + 4)[1:])
-    strings.append(f'{spacer[2:]}}}')
+            strings.extend(prepares_changes(node, indent=indent + 4))
+            strings.append(f'{spacer}  }}')
     return strings
 
 
 def format_stylish(diff: dict) -> str:
-    changes = []
-    changes.extend(prepares_changes(diff))
-    return '\n'.join(changes)
+    changes = '\n'.join(prepares_changes(diff))
+    return f'{{\n{changes}\n}}'
