7fc034fe04b123f2768206e7722c646070083740
Anton <zavr35rus@gmail.com>
2021-01-18 11:04:23 +0300

    File parsing and reading are separated

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -1,22 +1,12 @@
 import json
-import pathlib
-
 import yaml
 
 
-def get_data(filepath):
-    path = get_path(filepath)
-    extension = path.suffix
-    with path.open() as file:
+def get_data(file, extension):
+    with file.open() as file:
         if extension == '.json':
             return json.load(file)
         elif extension == '.yml':
             return yaml.safe_load(file)
         else:
             raise ValueError("Unsupported file extension")
-
-
-def get_path(path):
-    based_path = pathlib.Path().cwd()
-    absolute_path = based_path.joinpath(path)
-    return absolute_path
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,10 +1,20 @@
-from gendiff.file_parser import get_data
+import pathlib
+
 from gendiff.cli import formatter
+from gendiff.file_parser import get_data
 from .tree import get_diff
 
 
+def read_file(filepath: str) -> tuple:
+    file = pathlib.Path(filepath)
+    extension = file.suffix
+    return file, extension
+
+
 def generate_diff(path_file1, path_file2, style='stylish'):
-    data1 = get_data(path_file1)
-    data2 = get_data(path_file2)
+    file1, extension1 = read_file(path_file1)
+    file2, extension2 = read_file(path_file2)
+    data1 = get_data(file1, extension1)
+    data2 = get_data(file2, extension2)
     diff = get_diff(data1, data2)
     return formatter(diff, style)
